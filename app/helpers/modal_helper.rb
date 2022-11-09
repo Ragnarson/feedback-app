8ff83bb3818796b1de1f4ff2bdd9c24c9af919a8
JakubSQ <skuta.jakub@gmail.com>
2022-11-09 11:16:22 +0100

    updated guards on feedback buttons

--- app/helpers/modal_helper.rb
+++ app/helpers/modal_helper.rb
@@ -1,6 +1,9 @@
 module ModalHelper
-  def link_to_modal(text, modal_id, class_model, recipient_id, **options, &block)
-    return if class_model.where(user: current_user, recipient: recipient_id).any?
+  def link_to_modal(text, modal_id, class_model_1, class_model_2=nil, recipient_id, **options, &block)
+    if class_model_2.present?
+      return if class_model_2.where(user: current_user, recipient: recipient_id).any?
+    end
+    return if class_model_1.where(user: current_user, recipient: recipient_id).any?
 
     content_tag(:button, text, onclick: "toggleModal(#{modal_id.to_json}, event)", **options, &block)
   end
